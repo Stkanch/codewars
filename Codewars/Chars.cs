e1f9152930f7a4512fb24373d5155024b656047e
Stepan Poznyak <boga_pba@mail.ru>
2023-07-05 14:34:53 +0300

    Chars resolve

---
+++ Codewars/Chars.cs
@@ -0,0 +1,11 @@
+
+namespace Codewars;
+
+public class Chars
+{
+    public static char GetChar(int charcode)
+    {
+        char i = (char)charcode;
+        return i;
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -3,5 +3,6 @@
 var n = NoBoring.NoBoringZeros(14600);
 Console.WriteLine(n);
 
-var sum = Kata.SumMul(0, 26);
-Console.WriteLine(sum);
+
+var a = Chars.GetChar(charcode: 65);
+Console.WriteLine(a);
